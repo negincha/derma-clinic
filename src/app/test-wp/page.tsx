cd62c0a300c00268d6b39a46d24cfcb005a9f60d
negincha <negin.cha44@gmail.com>
2026-06-04 04:16:48 -0700

    fix test page to check routing

--- src/app/test-wp/page.tsx
+++ src/app/test-wp/page.tsx
@@ -1,18 +1,8 @@
 export default async function TestPage() {
-  let data = null;
-  try {
-    const res = await fetch("https://derma-clinic.ir/?graphql", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        query: `query { generalSettings { title } }`,
-      }),
-      cache: 'no-store', // این خیلی مهم است
-    });
-    data = await res.json();
-  } catch (error) {
-    data = { error: "اتصال برقرار نشد" };
-  }
-  
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return (
+    <div>
+      <h1>تست ارتباط با سرور</h1>
+      <p>اگر این صفحه را می‌بینید، یعنی مسیردهی درست است و مشکل فقط از دریافتِ دیتا از وردپرس است.</p>
+    </div>
+  );
 }

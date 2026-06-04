23ce13fdd6f217a1b3c31d384620a720fc129937
negincha <negin.cha44@gmail.com>
2026-06-04 03:24:15 -0700

    clean config for nextjs on netlify

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // این تنظیمات باعث می‌شود روت‌های شما در نتلیفای به درستی شناخته شوند
-  output: 'standalone', 
-}
-
-module.exports = nextConfig

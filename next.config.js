58ce98de9e107e78bd4b0e643bbe348ef7658ec4
negincha <negin.cha44@gmail.com>
2026-06-04 03:20:55 -0700

    add next config for routing

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  // این تنظیمات باعث می‌شود روت‌های شما در نتلیفای به درستی شناخته شوند
+  output: 'standalone', 
 }
 
 module.exports = nextConfig
